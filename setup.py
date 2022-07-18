91daf2f040c0d75db493a7ccf6c6f09b98bcd951
a.whit <nml@whit.contact>
2022-07-18 17:28:51 -0400

    Initial commit of a ROS2 package that provides an interface for treating ROS2 parameter groups like Python collection data types.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'ros_parameter_collections'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='a.whit',
+    maintainer_email='ros@whit.contact',
+    description='Wrappers for ROS2 nodes that provide an interface for treating ROS2 parameter groups like Python collection data types.',
+    license='Mozilla Public License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
